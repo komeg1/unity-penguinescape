32f0d4290527cbdd09a70864699d1147f347b973
philipgoclever@gmail.com <>
2022-12-03 15:25:14 +0100

    Wrzucam stan z labów

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PlayerLife : MonoBehaviour
 {
 
     [SerializeField] private TMPro.TextMeshProUGUI deathText;
+    [SerializeField] private GameObject lifeCounterTextObject;
+    [SerializeField] public int health = 1;
+    public TMPro.TextMeshProUGUI lifeCounterText;
+
     private Animator animator = null;
     private PlayerMovement movementScript;
-
+    private SpriteRenderer renderer;
+    public string healthCounterPrefix = "Health: ";
     void Start()
     {
+        lifeCounterText = lifeCounterTextObject.GetComponent<TMPro.TextMeshProUGUI>();
         deathText.gameObject.SetActive(false);
         animator = GetComponent<Animator>();
         movementScript = GetComponent<PlayerMovement>();
+        renderer = GetComponent<SpriteRenderer>();
+
+        lifeCounterText.SetText(healthCounterPrefix + health);
     }
 
     // Update is called once per frame
@@ -22,31 +31,70 @@ public class PlayerLife : MonoBehaviour
         
     }
 
-    IEnumerator Death()
+    public void Hurt()
+    {
+        Debug.Log("Hurt");
+        health -= 1;
+        lifeCounterText.SetText(healthCounterPrefix + health);
+        if (health <= 0)
+            Death();
+    }
+
+    public void Death()
+    {
+        animator.SetBool("died", true);
+        StartCoroutine(DeathCoroutine());
+    }
+    IEnumerator DeathCoroutine()
     {
         movementScript.canMove = false;
         GetComponent<Rigidbody2D>().isKinematic = true;
         yield return new WaitForSeconds(1);
 
-        gameObject.SetActive(false);
+       
         deathText.gameObject.SetActive(true);
+        StartCoroutine(ReloadScene());
+        renderer.forceRenderingOff = true;
+        //gameObject.SetActive(false);
+
+    }
+    IEnumerator ReloadScene()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        Resources.UnloadUnusedAssets();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("DamageSource"))
         {
-            animator.SetBool("died", true);
-            StartCoroutine(Death());
+            Hurt();
+        }
+        else if (collision.CompareTag("KillableDamageSource"))
+        {
+            Debug.Log("Killable enemy contact");
+            if (transform.position.y > collision.transform.position.y)
+            {
+                collision.gameObject.GetComponent<SpriteDeathScript>().Death();
+            }
+            else
+            {
+                Hurt();
+            }
+        }
+        else if (collision.CompareTag("InstaDeath"))
+        {
+            health = 0;
+            Hurt();
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("DamageSource"))
         {
-            animator.SetBool("died", true);
-            Debug.Log("Hit!");
-            StartCoroutine(Death());
+            Hurt();
         }
     }
 }
--- Assets/Scripts/PlayerScore.cs
+++ Assets/Scripts/PlayerScore.cs
@@ -5,23 +5,30 @@ using UnityEngine;
 public class PlayerScore : MonoBehaviour
 {
     private int score = 0;
+    private int keys = 0;
     private int coinScore = 10;
-    private int winScore = 100;
+    [SerializeField] private int winKeys = 3;
 
     private TMPro.TextMeshProUGUI scoreText;
-    [SerializeField] private GameObject textObject;
-    [SerializeField] private GameObject winTextObject;
-    [SerializeField] private GameObject finishScoreObject;
+    private TMPro.TextMeshProUGUI keysText;
+    [SerializeField] private GameObject scoreCounterTextObject;
+    [SerializeField] private GameObject keysCounterTextObject;
+    [SerializeField] private GameObject winScreenTextObject;
+    [SerializeField] private GameObject keysNeededToWinTextObject;
 
     private string scorePrefix = "Score: ";
-    private string finishScorePrefix = "Score needed: ";
+    private string keysPrefix = "Keys: ";
+    private string keysNeededToWinTextPrefix = "Keys needed: ";
     // Update is called once per frame
     void Start()
     {
-        scoreText = textObject.GetComponent<TMPro.TextMeshProUGUI>();
+        scoreText = scoreCounterTextObject.GetComponent<TMPro.TextMeshProUGUI>();
+        keysText = keysCounterTextObject.GetComponent<TMPro.TextMeshProUGUI>();
+
         scoreText.SetText(scorePrefix + score);
-        finishScoreObject.GetComponent<TMPro.TextMeshProUGUI>().SetText(finishScorePrefix + winScore);
-        winTextObject.active = false;
+        keysText.SetText(keysPrefix + score);
+        keysNeededToWinTextObject.GetComponent<TMPro.TextMeshProUGUI>().SetText(keysNeededToWinTextPrefix + winKeys);
+        winScreenTextObject.active = false;
 
     }
 
@@ -32,13 +39,25 @@ public class PlayerScore : MonoBehaviour
             other.GetComponent<CoinScript>().pickedUp();
             score += coinScore;
             scoreText.SetText(scorePrefix + score);
+        }else if (other.CompareTag("Key"))
+        {
+            other.GetComponent<CoinScript>().pickedUp();
+            keys += 1;
+            keysText.SetText(keysPrefix + keys);
         }
         else if(other.CompareTag("Finish Line"))
         {
-            if(score >= winScore)
+            if(keys >= winKeys)
             {
-                winTextObject.active = true;
+                winScreenTextObject.active = true;
             }
+        }
+        else if (other.CompareTag("Life"))
+        {
+            PlayerLife pl = GetComponent<PlayerLife>();
+            pl.health += 1;
+            pl.lifeCounterText.SetText(pl.healthCounterPrefix + pl.health);
+            other.GetComponent<CoinScript>().pickedUp();
         }
     }
 }
